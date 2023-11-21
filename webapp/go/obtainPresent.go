77bf8b2a1fb8c7d07b9b615775ca40e5b4b56f77
EkeMinusYou <euonymuslke@gmail.com>
2023-11-22 04:12:04 +0900

    obtainPresent: join receive history

--- webapp/go/obtainPresent.go
+++ webapp/go/obtainPresent.go
@@ -5,39 +5,34 @@ import "github.com/jmoiron/sqlx"
 // obtainPresent プレゼント付与
 func (h *Handler) obtainPresent(tx *sqlx.Tx, userID int64, requestAt int64) ([]*UserPresent, error) {
 	normalPresents := make([]*PresentAllMaster, 0)
-	query := "SELECT * FROM present_all_masters WHERE registered_start_at <= ? AND registered_end_at >= ?"
-	if err := tx.Select(&normalPresents, query, requestAt, requestAt); err != nil {
+	query := `
+SELECT
+  pam.ID AS id,
+  pam.registered_start_at AS registered_start_at,
+  pam.registered_end_at AS registered_end_at,
+  pam.item_type AS item_type,
+  pam.item_id AS item_id,
+  pam.amount AS amount,
+  pam.present_message AS present_message,
+  pam.created_at AS created_at
+FROM
+  present_all_masters pam
+  LEFT JOIN user_present_all_received_history uparh ON pam.id = uparh.present_all_id AND uparh.user_id = ?
+WHERE
+  pam.registered_start_at <= ? AND
+  pam.registered_end_at >= ? AND
+  uparh.ID IS NULL
+`
+	if err := tx.Select(&normalPresents, query, userID, requestAt, requestAt); err != nil {
 		return nil, err
 	}
 
 	obtainPresents := make([]*UserPresent, 0)
 
-	receivedList := make([]*UserPresentAllReceivedHistory, 0)
-	var presentIds []int64
-	for _, np := range normalPresents {
-		presentIds = append(presentIds, np.ID)
-	}
-	query, args, err := sqlx.In("SELECT * FROM user_present_all_received_history WHERE user_id=? AND present_all_id IN (?)", userID, presentIds)
-	if err != nil {
-		return nil, err
-	}
-	err = tx.Select(&receivedList, query, args...)
-	if err != nil {
-		return nil, err
-	}
-
 	upList := make([]UserPresent, 0)
 	historyList := make([]UserPresentAllReceivedHistory, 0)
 
-L1:
 	for _, np := range normalPresents {
-		// プレゼント配布済
-		for _, received := range receivedList {
-			if received.PresentAllID == np.ID {
-				continue L1
-			}
-		}
-
 		pID, err := h.generateID()
 		if err != nil {
 			return nil, err
