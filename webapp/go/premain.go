b858d5e60acdbe960a6f5656aeb96d5062d1b606
EkeMinusYou <euonymuslke@gmail.com>
2023-11-23 18:22:44 +0900

    add fgpprof

--- webapp/go/premain.go
+++ webapp/go/premain.go
@@ -17,46 +17,61 @@ var fcpuprofile = flag.String("fcpuprofile", "", "write fgprof cpu profile to fi
 func main() {
 	flag.Parse()
 
+	var cpuCloser func()
+	var stopFGProf func() error
+
 	if *cpuprofile != "" {
-		cpu, err := os.Create(*cpuprofile)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		var fcpu *os.File
-		var stop func() error
-		if *fcpuprofile != "" {
-			fcpu, err = os.Create(*fcpuprofile)
-			if err != nil {
-				log.Fatal(err)
-			}
-			stop = fgprof.Start(fcpu, fgprof.FormatPprof)
-			if err != nil {
-				log.Fatal(err)
-			}
-		}
-
-		err = pprof.StartCPUProfile(cpu)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		go func() {
-			sig := make(chan os.Signal, 1)
-			signal.Notify(sig, syscall.SIGTERM, syscall.SIGINT, syscall.SIGQUIT)
-			<-sig
-
-			pprof.StopCPUProfile()
-			cpu.Close()
-
-			if stop != nil {
-				stop()
-				fcpu.Close()
-			}
-
-			os.Exit(0)
-		}()
+		cpu, closer := startCPUProfile(*cpuprofile)
+		defer cpu.Close()
+		cpuCloser = closer
 	}
 
+	if *fcpuprofile != "" {
+		fcpu, stop := startFGProfile(*fcpuprofile)
+		defer fcpu.Close()
+		stopFGProf = stop
+	}
+
+	handleSignals(cpuCloser, stopFGProf)
 	Run()
+
+}
+
+func startCPUProfile(cpuprofile string) (*os.File, func()) {
+	cpu, err := os.Create(cpuprofile)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = pprof.StartCPUProfile(cpu)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return cpu, pprof.StopCPUProfile
+}
+
+func startFGProfile(fcpuprofile string) (*os.File, func() error) {
+	fcpu, err := os.Create(fcpuprofile)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	stop := fgprof.Start(fcpu, fgprof.FormatPprof)
+	return fcpu, stop
+}
+
+func handleSignals(cpuCloser func(), stopFGProf func() error) {
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, syscall.SIGTERM, syscall.SIGINT, syscall.SIGQUIT)
+	go func() {
+		<-sig
+		if cpuCloser != nil {
+			cpuCloser()
+		}
+		if stopFGProf != nil {
+			stopFGProf()
+		}
+		os.Exit(0)
+	}()
 }
